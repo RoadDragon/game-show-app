59e1290287101d98295514db778694f46f4b57c9
RoadDragon <lesliesnewemail@gmail.com>
2019-09-28 17:18:58 -0400

    Create start button event listener

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const missed = 0;
+const startButton = document.querySelector('.btn__reset');
+
+startButton.addEventListener('click', (e) => {
+    const startOverlay = document.getElementById('overlay');
+    startOverlay.style.display = 'none';
+})
