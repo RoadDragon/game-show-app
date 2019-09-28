abacb03b67669d98ee797530cce9bfab7f86bdad
RoadDragon <lesliesnewemail@gmail.com>
2019-09-28 17:42:05 -0400

    add phrases

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ startButton.addEventListener('click', (e) => {
     const startOverlay = document.getElementById('overlay');
     startOverlay.style.display = 'none';
 })
+
+// source: https://boards.straightdope.com/sdmb/archive/index.php/t-334013.html
+const phrases = [
+    "our princess is in another castle",  //super mario bros
+    "finish him",  //mortal kombat
+    "buy somethin will ya",  //zelda
+    "prepare to qualify",  //pole position
+    "may the way of the hero lead to the triforce" //zelda III
+]
